12b21f7a0fb8a5600bd2130bb75c9026649d5be2
mancilla67 <matias.mancillacesped@outlook.es>
2026-05-01 15:19:13 -0400

    cambio de esp

--- include/config.h
+++ include/config.h
@@ -13,27 +13,27 @@
 //#define CONTROL_SOFTWARE 
 
 // ID de este robot (1 al 5)
-#define MI_ROBOT_ID 1 
+#define MI_ROBOT_ID 2
 
 // ==========================================
 //               PINES HARDWARE
 // ==========================================
 
 // Encoders
-#define PIN_ENC_IZQ_A 0
-#define PIN_ENC_IZQ_B 1
-#define PIN_ENC_DER_A 4
-#define PIN_ENC_DER_B 3
+#define PIN_ENC_IZQ_A 32 //para c3 supermini 0
+#define PIN_ENC_IZQ_B 33 //para c3 supermini 1
+#define PIN_ENC_DER_A 25 //para c3 supermini 4
+#define PIN_ENC_DER_B 26 //para c3 supermini 3
 
 // Driver Motores DRV8833
-#define MOT_IN1_PIN 5   
-#define MOT_IN2_PIN 6   
-#define MOT_IN3_PIN 7   
-#define MOT_IN4_PIN 10  
+#define MOT_IN1_PIN 16 //para c3 supermini 5   
+#define MOT_IN2_PIN 17 //para c3 supermini 6   
+#define MOT_IN3_PIN 18 //para c3 supermini 7   
+#define MOT_IN4_PIN 19 //para c3 supermini 10  
 
 // MPU
-#define PIN_I2C_SDA 8
-#define PIN_I2C_SCL 9
+#define PIN_I2C_SDA 21 //para c3 supermini 8
+#define PIN_I2C_SCL 22 //para c3 supermini 9
 
 // ==========================================
 //           PARÁMETROS DE CONTROL
