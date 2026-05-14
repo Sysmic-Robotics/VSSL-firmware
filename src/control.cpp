00406d56ee7e145295e778f4a7a887040b1fe50d
Nachojgm <ignacio.godoym@usm.cl>
2026-05-14 18:49:56 -0400

    Arreglo de cambios

--- src/control.cpp
+++ src/control.cpp
@@ -44,7 +44,6 @@ void updateControl() {
 
 
             // w = (1/r) * (v +/- (w*L)/2)
-<<<<<<< HEAD
             //setpointI = (1/WHEEL_RADIUS) * (linearCmd + (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
             //setpointD = (1/WHEEL_RADIUS) * (linearCmd - (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
             setpointI = g_Input_X * 2;
@@ -54,26 +53,6 @@ void updateControl() {
             // Entrada tipo joystick:
             // g_Input_Y = avance / retroceso
             // g_Input_X = giro izquierda / derecha
-=======
-            setpointI = (1/WHEEL_RADIUS) * (linearCmd + (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
-            setpointD = (1/WHEEL_RADIUS) * (linearCmd - (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
-            
-            //setpointI = g_Input_X * 3;
-            //setpointD = g_Input_Y * 3;
-        #else
-
-            // Entrada desde software: g_Input_X = velocidad lineal, g_Input_Y = velocidad angular
-            //double linearCmd = g_Input_X * 3.0;
-            //double angularCmd = g_Input_Y * 1.0;
-            // w = (1/r) * (v +/- (w*L)/2)
-            //setpointI = (1/WHEEL_RADIUS) * (linearCmd + (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
-            //setpointD = (1/WHEEL_RADIUS) * (linearCmd - (angularCmd * WHEEL_CENTER_DISTANCE) * 0.5);
-
-            double targetSpeed = g_Input_Y * 3;
-            setpointI = targetSpeed + (g_Input_X * 1.0);
-            setpointD = targetSpeed - (g_Input_X * 1.0);
-        #endif
->>>>>>> c3e0c2c990e54f61755b212a033e65335c8d050f
 
             const double LINEAR_GAIN  = 3.0;
             const double TURN_GAIN    = 3.0;
